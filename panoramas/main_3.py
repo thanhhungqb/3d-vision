3a471c611a84baff70cbb41ef1068bf45d1bfc8f
hung <thanhhungqb@gmail.com>
2019-10-07 09:30:23 +0900

    move functions to util and call

--- panoramas/main_3.py
+++ panoramas/main_3.py
@@ -3,20 +3,8 @@ from PIL import Image
 from numpy import *
 from scipy.misc import imsave
 
+import util
 from ref import sift, homography, warp
-
-
-def read_features_from_file(filename):
-    """ Read feature properties and return in matrix form. """
-
-    with open(filename, "r") as f:
-        contents = f.read()
-    a = contents.split()
-    a = [float(o) for o in a]
-    a = array(a[2:])  # remove 2 two index
-    a = a.reshape((-1, 4 + 128))
-    return a[:, :4], a[:, 4:]  # feature locations, descriptors
-
 
 N = 3
 h = 'lake_500'
@@ -26,29 +14,19 @@ l = {}
 d = {}
 for i in range(N):
     sift.process_image(imname[i], featname[i])
-    l[i], d[i] = read_features_from_file(featname[i])
+    l[i], d[i] = util.read_features_from_file(featname[i])
 
 matches = {}
 for i in range(N - 1):
     matches[i] = sift.match(d[i + 1], d[i])
 
-
-def convert_points(j):
-    ndx = matches[j].nonzero()[0]
-
-    fp = homography.make_homog(l[j + 1][ndx, :2].T)
-    ndx2 = [int(matches[j][i]) for i in ndx]
-    tp = homography.make_homog(l[j][ndx2, :2].T)
-    return fp, tp
-
-
 # estimate the homographies
 model = homography.RansacModel()
-fp, tp = convert_points(1)
+fp, tp = util.convert_points(matches, l, 1)
 # H_12 = homography.H_from_ransac(fp, tp, model)[0]  # im 1 to 2
 H_12 = homography.H_from_ransac(tp, fp, model)[0]  # im 2 to 1 (Reverse)
 
-fp, tp = convert_points(0)
+fp, tp = util.convert_points(matches, l, 0)
 H_01 = homography.H_from_ransac(fp, tp, model)[0]  # im 0 to 1
 
 # warp the images
