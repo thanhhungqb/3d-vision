e98daad81779cfeb588919293f5923db7a439aae
hung <thanhhungqb@gmail.com>
2019-10-07 09:29:50 +0900

    move some  functions to util

--- panoramas/util.py
+++ panoramas/util.py
@@ -1,6 +1,8 @@
 from PIL import Image
 from pylab import *
 
+from ref import homography
+
 
 def sift_show(imagename, resultname):
     """ Process an image and save the results in a file. """
@@ -15,6 +17,27 @@ def sift_show(imagename, resultname):
 
     os.system(cmmd)
     print 'processed', imagename, 'to', resultname
+
+
+def read_features_from_file(filename):
+    """ Read feature properties and return in matrix form. """
+
+    with open(filename, "r") as f:
+        contents = f.read()
+    a = contents.split()
+    a = [float(o) for o in a]
+    a = array(a[2:])  # remove 2 two index
+    a = a.reshape((-1, 4 + 128))
+    return a[:, :4], a[:, 4:]  # feature locations, descriptors
+
+
+def convert_points(matches, l, j):
+    ndx = matches[j].nonzero()[0]
+
+    fp = homography.make_homog(l[j + 1][ndx, :2].T)
+    ndx2 = [int(matches[j][i]) for i in ndx]
+    tp = homography.make_homog(l[j][ndx2, :2].T)
+    return fp, tp
 
 
 if __name__ == '__main__':
